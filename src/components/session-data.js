b5a404a5605d6b6630307d81d4f373665faffd2b
ThomasChun <tchun2055@gmail.com>
2019-01-10 19:10:48 -1000

    removed unused variables

--- src/components/session-data.js
+++ src/components/session-data.js
@@ -6,8 +6,7 @@ import SessionChart from './session-chart';
 export default class SessionData extends React.Component {
   render() {
     const { correct, incorrect, createdAt, updatedAt } = this.props;
-    let total = correct + incorrect;
-    const percentage = ((correct * 100 / total) || 0).toFixed(2);
+    
     return(
       <div className="session-data">
         <DisplayTime className="session-time" createdAt={createdAt} updatedAt={updatedAt}/>
