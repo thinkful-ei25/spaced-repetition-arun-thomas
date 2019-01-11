546ea641315704954c7015b023644044bdc1fc93
Arun Seehra <seehra.a@gmail.com>
2019-01-10 22:59:37 -0600

    Add logo to header
    Include a new "Learn" link as well

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,44 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import './header-bar.css';
-import Button, { THEMES as buttonThemes }from './button';
+import Button, { THEMES as buttonThemes } from './button';
 import { logout as logoutAction } from '../actions/auth';
 
 export function HeaderBar({ logout, user }) {
-  // Only render the top bar if we're logged in
-  if (!user) {
-    return null;
-  }
-
-  const logOutButton = <Button theme={buttonThemes.DANGER} onClick={() => logout()}>Log out</Button>;
-  const name = [user.firstName, user.lastName].join(' ') || user.username;
+  const logOutButton = (
+    <Button theme={buttonThemes.DANGER} onClick={() => logout()}>
+      Log out
+    </Button>
+  );
 
   return (
     <nav className="HeaderBar">
-      <NavLink
-        to="/dashboard"
-        className="HeaderBar_link"
-        activeClassName="HeaderBar_link--active"
-      >
-        {name}
-      </NavLink>
-      <NavLink
-        to="/history"
-        className="HeaderBar_link"
-        activeClassName="HeaderBar_link--active"
-      >
-        History
-      </NavLink>
-      <NavLink
-        to="/session-history"
-        className="HeaderBar_link"
-        activeClassName="HeaderBar_link--active"
-      >
-        Session History
-      </NavLink>
-      {logOutButton}
+      <Link to={user ? '/dashboard' : '/'} className="HeaderBar_link">
+        <img
+          src="logo-light.png"
+          alt="Spaced Latency Logo"
+          className="HeaderBar_logo"
+        />
+      </Link>
+
+      {user && (
+        <Fragment>
+          <NavLink
+            to="/"
+            className="HeaderBar_link"
+            activeClassName="HeaderBar_link--active"
+          >
+            Learn
+          </NavLink>
+          <NavLink
+            to="/history"
+            className="HeaderBar_link"
+            activeClassName="HeaderBar_link--active"
+          >
+            History
+          </NavLink>
+          <NavLink
+            to="/session-history"
+            className="HeaderBar_link"
+            activeClassName="HeaderBar_link--active"
+          >
+            Session History
+          </NavLink>
+          {logOutButton}
+        </Fragment>
+      )}
     </nav>
   );
 }
