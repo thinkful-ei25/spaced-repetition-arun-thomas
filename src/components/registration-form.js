8b3c6a9bd3977df895c51ccbfc8a37503ecb5704
Arun Seehra <seehra.a@gmail.com>
2019-01-10 21:08:55 -0600

    Remove extranous div in Registration form

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Field, reduxForm, focus } from 'redux-form';
 
+import './registration-form.css';
 import Button, { THEMES as buttonThemes } from './button';
 import Input from './input';
 import statelessWrapper from './stateless-wrapper';
 import { registerUser } from '../actions/users';
 import { login } from '../actions/auth';
-import { required, nonEmpty, matches, length, isTrimmed } from '../validators'; 
+import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
 
 const passwordLength = length({ min: 10, max: 72 });
 const matchesPassword = matches('password');
@@ -23,52 +24,56 @@ export class RegistrationForm extends React.Component {
 
   render() {
     return (
-      <div className="RegistrationForm">
-        <form
-          className="RegistrationForm_form"
-          onSubmit={this.props.handleSubmit((values) => this.onSubmit(values))}
-        >
-          <Field component={Input} type="text" name="firstName" label="First name" />
-          <Field component={Input} type="text" name="lastName" label="Last name" />
-          <Field
-            component={Input}
-            type="text"
-            name="username"
-            label="Username"
-            validate={[required, nonEmpty, isTrimmed]}
-          />
-          <Field
-            component={Input}
-            type="password"
-            name="password"
-            label="Password"
-            validate={[required, passwordLength, isTrimmed]}
-          />
-          <Field
-            component={Input}
-            type="password"
-            name="passwordConfirm"
-            label="Confirm password"
-            validate={[required, nonEmpty, matchesPassword]}
-          />
+      <form
+        className="RegistrationForm"
+        onSubmit={this.props.handleSubmit((values) => this.onSubmit(values))}
+      >
+        <Field component={Input} type="text" name="firstName" label="First name" />
+        <Field component={Input} type="text" name="lastName" label="Last name" />
+        <Field
+          component={Input}
+          type="text"
+          name="username"
+          label="Username"
+          validate={[required, nonEmpty, isTrimmed]}
+        />
+        <Field
+          component={Input}
+          type="password"
+          name="password"
+          label="Password"
+          validate={[required, passwordLength, isTrimmed]}
+        />
+        <Field
+          component={Input}
+          type="password"
+          name="passwordConfirm"
+          label="Confirm password"
+          validate={[required, nonEmpty, matchesPassword]}
+        />
 
-          <Button theme={buttonThemes.SUCCESS} type="submit" disabled={this.props.pristine || this.props.submitting}>
-            Register
-          </Button>
-          <Link to="/">Login</Link>
-        </form>
-      </div>
+        <Button
+          theme={buttonThemes.SUCCESS}
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+        >
+          Register
+        </Button>
+        <Link to="/">Login</Link>
+      </form>
     );
   }
 }
 
-/* 
+/*
  * It is necessary to wrap our reduxForm component because it uses forwardRefs *
  * which are not supported by the current verison of React Router.
  * See: https://github.com/erikras/redux-form/issues/4318
  */
-export default statelessWrapper(reduxForm({
-  form: 'registration',
-  onSubmitFail: (errors, dispatch) =>
-    dispatch(focus('registration', Object.keys(errors)[0])),
-})(RegistrationForm));
+export default statelessWrapper(
+  reduxForm({
+    form: 'registration',
+    onSubmitFail: (errors, dispatch) =>
+      dispatch(focus('registration', Object.keys(errors)[0])),
+  })(RegistrationForm)
+);
